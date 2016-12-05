6c51814097977919e5d95ef2e71103743aa83e4b
= <=>
2016-12-05 17:11:22 +0100

    indentation

--- day5_part1.rb
+++ day5_part1.rb
@@ -1,19 +1,19 @@
 require 'digest/md5'
 
 def generate(door_id)
-puts "generating password"
-current = 0
+  puts "generating password"
+  current = 0
 
-pass = ""
-while (pass.length < 8)
-  input = "#{door_id}#{current}"
-  md5 = Digest::MD5.hexdigest(input)
-  if (md5.match(/^00000/))
-    pass += md5.chars.to_a[5].to_s
-    puts pass
+  pass = ""
+  while (pass.length < 8)
+    input = "#{door_id}#{current}"
+    md5 = Digest::MD5.hexdigest(input)
+    if (md5.match(/^00000/))
+      pass += md5.chars.to_a[5].to_s
+      puts pass
+    end
+    current += 1
   end
-  current += 1
-end
 end
 
 #test_id = "abc"
--- day5_part2.rb
+++ day5_part2.rb
@@ -1,19 +1,19 @@
 require 'digest/md5'
 
 def generate(door_id)
-start = Time.now
-prefix = "0" * 5
-puts "generating password"
-current = 0
-found = 0
-pass = ["-","-","-","-","-","-","-","-"]
-while (found < 8)
-  input = "#{door_id}#{current}"
-  md5 = Digest::MD5.hexdigest(input)
-  if (md5.start_with?(prefix))
-    chars = md5.chars.to_a
-    position = chars[5].to_s
-    if (position.match(/[0-7]/))
+  start = Time.now
+  prefix = "0" * 5
+  puts "generating password"
+  current = 0
+  found = 0
+  pass = ["-","-","-","-","-","-","-","-"]
+  while (found < 8)
+    input = "#{door_id}#{current}"
+    md5 = Digest::MD5.hexdigest(input)
+    if (md5.start_with?(prefix))
+      chars = md5.chars.to_a
+      position = chars[5].to_s
+      if (position.match(/[0-7]/))
         if (pass[position.to_i] == "-")
           value = chars[6]
           pass[position.to_i] = value
@@ -21,10 +21,10 @@ while (found < 8)
           puts pass.join
         end
       end
+    end
+    current += 1
   end
-  current += 1
-end
-puts "#{current} iterations in #{Time.now-start} seconds needed"
+  puts "#{current} iterations in #{Time.now-start} seconds needed"
 end
 
 #test_id = "abc"
