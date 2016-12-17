fca0e6fbca02bcd301f949e433a245ca0f214268
Peter de Kraker <peter@umito.nl>
2016-12-17 23:32:05 +0100

    day 17

---
+++ day17_part1.rb
@@ -0,0 +1,87 @@
+require 'digest/md5'
+class Route
+
+  attr_accessor :x,:y,:steps
+
+  def initialize(x,y)
+    @x = x
+    @y = y
+    @steps = []
+  end
+
+  def move(x,y)
+    if (x == @x)
+      if (y > @y)
+        @steps << "D"
+      elsif (y < @y)
+        @steps << "U"
+      end
+    elsif(y == @y)
+      if (x > @x)
+        @steps << "R"
+      elsif (x < @x)
+        @steps << "L"
+      end
+    end
+    @x = x
+    @y = y
+  end
+
+  def dup
+    dup = Route.new(@x,@y)
+    dup.steps = @steps.dup
+    return dup
+  end
+end
+
+def next_steps(route)
+  #up down left right
+  @up = [0,-1]
+  @down = [0,1]
+  @left = [-1,0]
+  @right = [1,0]
+  positions = [@up,@down, @left, @right].map{|(x,y)| [route.x + x, route.y + y]}
+  hash = hash(route.steps.join)
+  open = positions.each_with_index.select{|p,i| hash[i].match(/[b-f]/)}.map{|p,i|p}
+  valid = open.select{|(x,y)|x >= 0 && x < 4 && y >= 0 && y < 4}
+  return valid
+end
+
+def process
+  puts $key
+  solve(3,3,[Route.new(0,0)])
+end
+
+def solve(x,y, routes)
+  next_routes = []
+  routes.each do |route|
+    next_steps = next_steps(route)
+    next_steps.each do |step|
+      dup = route.dup
+      dup.move(step[0],step[1])
+      next_routes << dup
+      if (step[0] == 3 && step[1] == 3)
+        puts "Reached 3,3 in #{dup.steps.length} steps: #{dup.steps.join}"
+        return
+      end
+    end
+  end
+  solve(x,y,next_routes)
+end
+
+
+def hash(path)
+  Digest::MD5.hexdigest("#{$key}#{path}")
+end
+
+$key = "ihgpwlah"
+process
+
+$key = "kglvqrro"
+process
+
+$key = "ulqzkmiv"
+process
+
+$key = "ioramepc"
+process
